fa774508cd1df7fa718381801445b2c86074408c
Bassem <bassem.alpha@gmail.com>
2018-06-13 11:52:29 +0200

    design fixes
    design fixes

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,1 @@
-"use strict";$("#hamburger-btn").click(function(){var e=$("#nav");e.hasClass("js-opened")?e.removeClass("js-opened"):e.addClass("js-opened")}),$(".side-menu-btn").click(function(){$(".side-menu").toggle(100)});
+"use strict";$(".side-menu-btn").click(function(){$(".side-menu").toggle(100)});
--- src/js/script.js
+++ src/js/script.js
@@ -10,31 +10,6 @@
 
 'use strict';
 
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
- *  Navigation
- *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-//
-// Open/Close navigation
-//
-$('#hamburger-btn').click(function() {
-  
-  var $nav = $('#nav');
-
-  if ($nav.hasClass('js-opened')) {
-
-    $nav.removeClass('js-opened');
-
-  } else {
-
-    $nav.addClass('js-opened');
-
-  }
-});
-
 //
 // Open/Close side menu
 //
