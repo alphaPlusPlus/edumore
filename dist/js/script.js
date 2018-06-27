8f75b1b80b0644b6b703d0f99a6bf0087b90b907
Bassem <bassem.alpha@gmail.com>
2018-06-27 09:41:20 +0200

    updates

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,1 @@
-"use strict";$(".side-menu-btn").click(function(){$(".side-menu").toggle(100)});
+"use strict";$(".side-menu-btn").click(function(){$(".side-menu").toggle(100)}),$(".table-container").width($(window).width()-20),$(window).resize(function(){$(".table-container").width($(window).width()-20)});
