15d2b6cc64efd7afbb52fe031369dc276c3b1e28
Bassem <bassem.alpha@gmail.com>
2018-06-27 09:28:03 +0200

    new fixes

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,9 @@ $('.side-menu-btn').click(function() {
   var $sidemenu = $('.side-menu');
   $sidemenu.toggle(100);
 });
+
+$('.table-container').width($( window ).width() - 20);
+
+$( window ).resize(function() {
+  $('.table-container').width($( window ).width() - 20);
+});
